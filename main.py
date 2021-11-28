8d28d816da4710a89f9e26bf34137b75380d68ba
VIkaskashyap307 <90996037+VIkaskashyap307@users.noreply.github.com>
2021-11-28 12:51:39 -0500

    2nd Commit

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from cmd import Cmd
 
 class MyPrompt(Cmd):
     prompt = 'Shell> '
-    intro = '''Welcome !!
-
-Please choose the number to run the application :
+    intro = '''Welcome to Big Data Processing Application
+Please type the number that corresponds to which application you would like to run:
 1. Apache Hadoop
 2. Apache Spark
 3. Jupyter Notebook
-4. Sonarqube And Sonarscanner '''
+4. Sonarqube And Sonarscanner 
+
+Type the number here >'''
 
    
-
     def do_hadoop (self,inp):
         print('Click on URL below for Apache Hadoop:')
-        link = "TBA"
+        link = "http://34.70.67.87:9870/dfshealth.html#tab-overview"
         print(link)
 
     def do_spark(self, inp):
@@ -24,12 +24,12 @@ Please choose the number to run the application :
 
     def do_jupyter(self, inp):
         print('Click on URL below for Jupyter Notebook:')
-        link = "http://104.154.151.198:8888/"
+        link = "http://104.154.151.198:8888/login?next=%2Ftree%3F"
         print(link)
 
     def do_sonar(self, inp):
         print('Click on URL below for SonarQube:')
-        link = "http://35.224.173.90:9000/"
+        link = "http://35.224.173.90:9000/sessions/new?return_to=%2F"
         print(link)
 
     def do_unrecognised(selfself,inp):
